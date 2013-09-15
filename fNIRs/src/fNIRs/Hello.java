aaf61d57b3c54e6d98c37bb5c7ec392c9e9a01e9
Sarah Hammond <shammond@hamilton.edu>
2013-09-15 19:07:09 -0400

    changed Hello

--- fNIRs/src/fNIRs/Hello.java
+++ fNIRs/src/fNIRs/Hello.java
@@ -6,6 +6,7 @@ import org.eclipse.swt.widgets.Button;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.events.SelectionAdapter;
 import org.eclipse.swt.events.SelectionEvent;
+import org.eclipse.swt.widgets.Label;
 
 public class Hello {
 
@@ -55,6 +56,10 @@ public class Hello {
 		});
 		btnPressMe.setBounds(164, 114, 75, 25);
 		btnPressMe.setText("Press Me!");
+		
+		Label lblHelloWorld = new Label(shell, SWT.NONE);
+		lblHelloWorld.setBounds(184, 63, 55, 15);
+		lblHelloWorld.setText("Hello, World!!");
 
 	}
 }
