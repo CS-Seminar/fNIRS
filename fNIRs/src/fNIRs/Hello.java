1ffaa974fafa65d0a38f55e133cafe1265558d53
Sarah Hammond <shammond@hamilton.edu>
2013-09-16 14:51:32 -0400

    better gui

--- fNIRs/src/fNIRs/Hello.java
+++ fNIRs/src/fNIRs/Hello.java
@@ -7,6 +7,7 @@ import org.eclipse.swt.SWT;
 import org.eclipse.swt.events.SelectionAdapter;
 import org.eclipse.swt.events.SelectionEvent;
 import org.eclipse.swt.widgets.Label;
+import org.eclipse.swt.widgets.Spinner;
 
 public class Hello {
 
@@ -48,18 +49,38 @@ public class Hello {
 		shell.setSize(450, 300);
 		shell.setText("SWT Application");
 		
+		final Label lblHelloWorld = new Label(shell, SWT.NONE);
+		lblHelloWorld.setBounds(164, 81, 75, 15);
+		lblHelloWorld.setText("Hello, World!!");
+		lblHelloWorld.setVisible(false);
+	
+		final Spinner spinner = new Spinner(shell, SWT.BORDER);
+		spinner.setBounds(65, 44, 47, 22);
+		
 		Button btnPressMe = new Button(shell, SWT.NONE);
 		btnPressMe.addSelectionListener(new SelectionAdapter() {
 			@Override
 			public void widgetSelected(SelectionEvent e) {
+				lblHelloWorld.setVisible(true);
+				lblHelloWorld.setText(spinner.getText());
 			}
 		});
 		btnPressMe.setBounds(164, 114, 75, 25);
 		btnPressMe.setText("Hey Sarah!");
 		
-		Label lblHelloWorld = new Label(shell, SWT.NONE);
-		lblHelloWorld.setBounds(184, 63, 55, 15);
-		lblHelloWorld.setText("Hello, World!!");
+		Button btnExit = new Button(shell, SWT.NONE);
+		btnExit.addSelectionListener(new SelectionAdapter() {
+			@Override
+			public void widgetSelected(SelectionEvent e) {
+				shell.close();
+			}
+		});
+		btnExit.setBounds(164, 194, 75, 25);
+		btnExit.setText("Exit");
+		
+	
+		
+
 
 
 
