19562cb5b33d9a5244dc89b472f4af86f3dac8d9
unknown <jssmith@CSLP-16803.hamilton.edu>
2013-10-28 22:01:16 -0400

    Introduce Rapid Driver. Delete Jamal. GTFO, Jamal

---
+++ fNIRs/src/fNIRs/RapidDriver.java
@@ -0,0 +1,40 @@
+package fNIRs;
+
+import java.io.*;
+
+import com.rapidminer.RapidMiner.ExecutionMode;
+import com.rapidminer.RapidMiner;
+import com.rapidminer.Process;
+import com.rapidminer.operator.OperatorException;
+import com.rapidminer.tools.XMLException;
+
+
+public class RapidDriver {
+
+	
+	public static void main(String[] argv) throws Exception {
+		//Running within another program		
+		RapidMiner.setExecutionMode(ExecutionMode.EMBEDDED_WITHOUT_UI);
+	    
+
+//		String rapidMinerHome = "C:\\Program Files\\Rapid-I\\RapidMiner5\\lib";
+		System.out.println(System.getProperty("user.dir"));
+		
+		//		String rapidMinerHome = "/fNIRs";
+//		System.setProperty("rapidminer.home", rapidMinerHome);
+		
+	
+		File myFile = new File("./src/fNirs/process");
+		System.out.print(myFile.getCanonicalPath());
+		// MUST BE INVOKED BEFORE ANYTHING ELSE !!!
+		RapidMiner.init();
+
+		
+	    // create the process from the command line argument file
+		Process process = new Process(myFile);
+
+	    // run the process on the input
+	    process.run();
+	}
+	
+}
